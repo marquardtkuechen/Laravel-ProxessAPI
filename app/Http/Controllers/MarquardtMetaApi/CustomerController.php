fbc638cd43f0de55d6e593f53bf3aa5f1d4f62ad
christianlinde <607323+chrisUse@users.noreply.github.com>
2021-05-05 08:54:18 +0200

    create dummy response for createCustomer

--- app/Http/Controllers/MarquardtMetaApi/CustomerController.php
+++ app/Http/Controllers/MarquardtMetaApi/CustomerController.php
@@ -15,16 +15,38 @@ class CustomerController extends Controller
      *     description="Customer",
      * )
      * /
-     *
-     */
-     /**
+     * /**
      * @OA\POST(
      *  path="/customer",
      *  tags={"Customer"},
      *  operationId="createCustomer",
      *  summary="",
      *  security={{"bearerAuth": {}}},
-
+     *
+     * @OA\RequestBody(@OA\MediaType(
+     *        mediaType="application/json",
+     *        @OA\Schema(
+     *           type="object",
+     *           @OA\Property(property="salutationKey", type="string", example=""),
+     *           @OA\Property(property="iso2A", type="string", example="DE"),
+     *           @OA\Property(property="federalStateKey", type="string", example=""),
+     *           @OA\Property(property="name2", type="string", example=""),
+     *           @OA\Property(property="name1", type="string", example=""),
+     *           @OA\Property(property="strasse", type="string", example=""),
+     *           @OA\Property(property="plz", type="string", example=""),
+     *           @OA\Property(property="ort", type="string", example=""),
+     *           @OA\Property(property="contractList", type="array",
+     *                          collectionFormat="multi",
+     *                          @OA\Items( type="string" ),
+     *                          example={"1","2","147"}),
+     *           @OA\Property(property="attentionList", type="array",
+     *                          collectionFormat="multi",
+     *                          @OA\Items( type="string" ),
+     *                          example={"1","2","147"}),
+     *        )
+     *     ),
+     *  ),
+     *
      *  @OA\Response(response="200",
      *    description="Validation Response",
      *      @OA\JsonContent()
@@ -37,8 +59,9 @@ class CustomerController extends Controller
      */
 
     public function createCustomer(Request $request){
-
-        return "";
+        $json = '{
+            "status": "OK"
+        }';
+        return response()->json(json_decode($json, true));
     }
-
 }
